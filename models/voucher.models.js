07a241ca0a29c9241453472c1bb7c147abe87207
Zatking <117702923+Zatking@users.noreply.github.com>
2024-07-30 01:02:26 +0700

    a

--- models/voucher.models.js
+++ models/voucher.models.js
@@ -144,44 +144,48 @@ const useHistory = new mongoose.Schema({
 
 
 const partNerRequestSchema = new mongoose.Schema({
-  OrderID:{
-    type: String,
-    required: [true, "Please enter the order ID"],
-  },
-  PartnerID:{
-    type: String,
-    required: [true, "Please enter the partner request ID"],
-  },
-  PartnerName:{
-    type: String,
-    required: [true, "Please enter the partner name"],
-  },
-  ServiceCode:{
-    type: String,
-    required: [true, "Please enter the service code"],
-  },
-  ServiceName:{
-    type: String,
-    required: [true, "Please enter the service name"],
-  },
-  TotalMoney:{
-    type: Number,
-    required: [true, "Please enter the total money"],
-  },
-  CustomerCode:{
-    type: String,
-    required: [true, "Please enter the customer code"],
-  },
-  CustomerName:{
-    type: String,
-    required: [true, "Please enter the customer name"],
-  },
-  Description:{
-    type: String,
-    required: [true, "Please enter the description"],
-  },
-  
-})
+  //---------->Cái api này sẽ truyền qua hiếu để hiếu lấy dữ liệu thanh toán lun
+OrderID: {
+  type: String,
+  required: [true, "Please enter the order ID"],
+},
+
+PartnerID: {
+  type: String,
+  required: [true, "Please enter the partner request ID"],
+},
+
+ServiceName: {
+  type: String,
+  required: [true, "Please enter the service name"],
+},
+
+TotalMoney: {
+  type: Number,
+  required: [true, "Please enter the total money"],
+},
+
+CustomerCode: {
+  type: String,
+  required: [true, "Please enter the customer code"],
+},
+
+Description: {
+  type: String,
+  required: [true, "Please enter the description"],
+},
+
+LinkHome: {
+  type: String,
+  required: [true, "Please enter the link home"],
+},
+// ---------->lấy này để hiếu trả về trang của tụi tui
+LinkReturnSuccess: {
+  type: String,
+  required: [true, "Please enter the link return success"],
+},
+// ---------->lấy này để hiếu cập nhập trạng thái trang của tụi tui
+});
 
 const Voucher = mongoose.model("Voucher", voucherSchema);
 const Partner = mongoose.model("Partner", partNerSchema);
